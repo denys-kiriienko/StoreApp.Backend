a51efd9a98d929b22037ca582c134111446ea40b
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-06-16 12:57:11 +0300

    Add AppDbContext

---
+++ StoreApp.DAL/Data/AppDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoreApp.DAL.Entities;
+
+namespace StoreApp.DAL.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<UserEntity> Users { get; set; }
+        public DbSet<ProductEntity> Products { get; set; }
+        public DbSet<CartItemEntity> CartItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<UserEntity>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+
+            modelBuilder.Entity<CartItemEntity>()
+                .HasOne(ci => ci.User)
+                .WithMany(u => u.CartItems)
+                .HasForeignKey(ci => ci.UserId);
+
+            modelBuilder.Entity<CartItemEntity>()
+                .HasOne(ci => ci.Product)
+                .WithMany(p => p.CartItems)
+                .HasForeignKey(ci => ci.ProductId);
+
+            modelBuilder.Entity<ProductEntity>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(18,2)");
+        }
+    }
+}
--- StoreApp.DAL/Entities/ProductEntity.cs
+++ StoreApp.DAL/Entities/ProductEntity.cs
@@ -7,5 +7,7 @@
         public string? Description { get; set; }
         public required decimal Price { get; set; }
         public byte[]? ImageData { get; set; }
+
+        public ICollection<CartItemEntity> CartItems { get; set; } = new List<CartItemEntity>();
     }
 }
