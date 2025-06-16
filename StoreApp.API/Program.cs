dee7fc9f6f267387166a3a70fe812f2a85c06ccc
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-06-16 12:47:45 +0300

    Add database connection

--- StoreApp.API/Program.cs
+++ StoreApp.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using StoreApp.DAL.Data;
+
 namespace StoreApp.API
 {
     public class Program
@@ -5,6 +8,14 @@ namespace StoreApp.API
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+                ?? throw new InvalidOperationException("Connection string"
+                + "'DefaultConnection' not found.");
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
             var app = builder.Build();
 
             app.MapGet("/", () => "Hello World!");
