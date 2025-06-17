e13e530aa0009bf35a0eb51c108ce1f23155081e
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-06-17 12:47:13 +0300

    Add Auth constants

---
+++ StoreApp.Shared/Constants/AuthConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace StoreApp.Shared.Constants
+{
+    public static class AuthConstants
+    {
+        public const string AccessTokenCookie = "access_token";
+        public const string RefreshTokenCookie = "refresh_token";
+        public const int AccessTokenExpiresMinutes = 15;
+        public const int RefreshTokenExpiresDays = 7;
+    }
+}
