f22ca5c2ba4dd91ea335f54719531e838e0535fa
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-06-25 13:21:49 +0300

    Add fetchWithCredentials function

---
+++ StoreApp.Admin.Client/wwwroot/js/fetchWithCredentials.js
@@ -0,0 +1,18 @@
+﻿export async function fetchWithCredentials(url, options) {
+    options = options || {};
+    options.credentials = 'include';
+
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+        throw new Error('Fetch failed: ' + response.status);
+    }
+
+    const contentType = response.headers.get('content-type');
+
+    if (contentType && contentType.includes('application/json')) {
+        return await response.json();
+    } else {
+        return await response.text();
+    }
+}
