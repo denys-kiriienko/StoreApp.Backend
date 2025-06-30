710b4123613d4adba1489b373cb3971c5337fd0e
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-06-30 19:07:59 +0300

    Add Navbar component

---
+++ StoreApp.Admin.Client/Components/Navbar.razor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+using StoreApp.Admin.Client.Services;
+
+namespace StoreApp.Admin.Client.Components
+{
+    public partial class Navbar : ComponentBase, IDisposable
+    {
+        [Inject] public IAuthApiService AuthApiService { get; set; } = default!;
+        [Inject] public NavigationManager Navigation { get; set; } = default!;
+
+        protected bool isAuthenticated = false;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await AuthApiService.InitializeAsync();
+            AuthApiService.AuthStateChanged += OnAuthChanged;
+            StateHasChanged();
+        }
+
+        private void OnAuthChanged(bool auth)
+        {
+            InvokeAsync(StateHasChanged);
+        }
+
+        private void NavigateToProducts()
+        {
+            Navigation.NavigateTo("/Products");
+        }
+
+        private void NavigateToLogin()
+        {
+            Navigation.NavigateTo("/Login");
+        }
+
+        private async Task Logout()
+        {
+            await AuthApiService.LogoutAsync();
+        }
+
+        public void Dispose()
+        {
+            AuthApiService.AuthStateChanged -= OnAuthChanged;
+        }
+    }
+}
