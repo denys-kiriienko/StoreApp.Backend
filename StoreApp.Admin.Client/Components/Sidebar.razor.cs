758f14557c73beeab4ce6873449433105ff51a94
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-06-30 17:17:44 +0300

    Add Sidebar component

---
+++ StoreApp.Admin.Client/Components/Sidebar.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace StoreApp.Admin.Client.Components
+{
+    public partial class Sidebar : ComponentBase
+    {
+        [Parameter] public EventCallback<bool> OnSidebarToggle { get; set; }
+        [Parameter] public bool StartOpen { get; set; } = false;
+
+        protected bool isOpen = false;
+        protected bool dropdownOpen = false;
+
+        protected override void OnInitialized()
+        {
+            isOpen = StartOpen;
+        }
+
+        protected async Task Toggle()
+        {
+            isOpen = !isOpen;
+            await OnSidebarToggle.InvokeAsync(isOpen);
+        }
+
+        protected void ToggleDropdown()
+        {
+            dropdownOpen = !dropdownOpen;
+
+            if (!isOpen)
+                isOpen = true;
+        }
+    }
+}
