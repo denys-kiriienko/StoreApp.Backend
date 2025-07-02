6dfbb1a3eb87b584cb291757a2eb24d83a1d16ba
naxiner <kirienkodenis2002@gmail.com>
2025-07-02 13:17:15 +0300

    Refactor ProductApiService to use HttpClient

--- StoreApp.Admin.Client/Services/ProductApiService.cs
+++ StoreApp.Admin.Client/Services/ProductApiService.cs
@@ -1,45 +1,72 @@
 ﻿using StoreApp.Shared.Models;
+using System.Net.Http;
 using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 namespace StoreApp.Admin.Client.Services;
 
 public class ProductApiService : IProductApiService
 {
-    private readonly FetchInterop _fetchInterop;
     private readonly HttpClient _http;
-    private const string ApiPath = "https://localhost:7056/api/Product";
+    private const string ApiPath = "api/Product";
 
-    public ProductApiService(FetchInterop fetchInterop, HttpClient http)
+    public ProductApiService(HttpClient http)
     {
-        _fetchInterop = fetchInterop;
         _http = http;
     }
 
     public async Task<List<ProductModel>> GetAllProductsAsync()
     {
-        return await _fetchInterop.GetAsync<List<ProductModel>>(ApiPath);
+        var request = new HttpRequestMessage(HttpMethod.Get, ApiPath);
+        request.SetBrowserRequestCredentials(BrowserRequestCredentials.Include);
+
+        var response = await _http.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+
+        return await response.Content.ReadFromJsonAsync<List<ProductModel>>() ?? new();
     }
 
     public async Task<ProductModel?> GetProductByIdAsync(int id)
     {
-        return await _fetchInterop.GetAsync<ProductModel>($"{ApiPath}/{id}");
+        var request = new HttpRequestMessage(HttpMethod.Get, $"{ApiPath}/{id}");
+        request.SetBrowserRequestCredentials(BrowserRequestCredentials.Include);
+
+        var response = await _http.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+
+        return await response.Content.ReadFromJsonAsync<ProductModel>();
     }
 
     public async Task<bool> AddProductAsync(ProductModel product)
     {
-        var result = await _fetchInterop.PostAsync<object>(ApiPath, product);
-        return result != null;
+        var request = new HttpRequestMessage(HttpMethod.Post, ApiPath)
+        {
+            Content = JsonContent.Create(product)
+        };
+        request.SetBrowserRequestCredentials(BrowserRequestCredentials.Include);
+
+        var response = await _http.SendAsync(request);
+        return response.IsSuccessStatusCode;
     }
 
     public async Task<bool> UpdateProductAsync(int id, ProductModel product)
     {
-        var response = await _http.PutAsJsonAsync($"{ApiPath}/{id}", product);
+        var request = new HttpRequestMessage(HttpMethod.Put, $"{ApiPath}/{id}")
+        {
+            Content = JsonContent.Create(product)
+        };
+        request.SetBrowserRequestCredentials(BrowserRequestCredentials.Include);
+
+        var response = await _http.SendAsync(request);
         return response.IsSuccessStatusCode;
     }
 
     public async Task<bool> DeleteProductAsync(int id)
     {
-        var response = await _http.DeleteAsync($"{ApiPath}/{id}");
+        var request = new HttpRequestMessage(HttpMethod.Delete, $"{ApiPath}/{id}");
+        request.SetBrowserRequestCredentials(BrowserRequestCredentials.Include);
+
+        var response = await _http.SendAsync(request);
         return response.IsSuccessStatusCode;
     }
 }
