907f06339ecd5671ee6a9359523c0dae8addecef
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-07-04 13:23:28 +0300

    Clean code

--- StoreApp.Admin.Client/Components/Pages/Login/Login.razor.cs
+++ StoreApp.Admin.Client/Components/Pages/Login/Login.razor.cs
@@ -1,46 +1,45 @@
 ﻿using Microsoft.AspNetCore.Components;
 using StoreApp.Admin.Client.Services.Interfaces;
 
-namespace StoreApp.Admin.Client.Components.Pages.Login
+namespace StoreApp.Admin.Client.Components.Pages.Login;
+
+public partial class Login 
 {
-    public partial class Login 
+    [Inject] public required NavigationManager NavigationManager { get; set; }
+    [Inject] public required IAuthApiService AuthApiService { get; set; }
+
+    private string email = string.Empty;
+    private string password = string.Empty;
+    private bool showPassword;
+
+    private async Task HandleSubmitAsync()
     {
-        [Inject] public NavigationManager Navigation {  get; set; } = default!;
-        [Inject] public IAuthApiService AuthApiService { get; set; } = default!;
-
-        private string email = string.Empty;
-        private string password = string.Empty;
-        private bool showPassword = false;
-
-        private async Task HandleSubmit()
+        if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password))
         {
-            if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password))
-            {
-                Console.WriteLine("Email and password cannot be empty");
-                return;
-            }
-
-            try
-            {
-                var success = await AuthApiService.LoginAsync(email, password);
-                if (success)
-                {
-                    Navigation.NavigateTo("/products");
-                }
-                else
-                {
-                    Console.WriteLine("Login failed");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Login error: {ex.Message}");
-            }
+            Console.WriteLine("Email and password cannot be empty");
+            return;
         }
 
-        private void TogglePasswordVisibility()
+        try
         {
-            showPassword = !showPassword;
+            var success = await AuthApiService.LoginAsync(email, password);
+            if (success)
+            {
+                NavigationManager.NavigateTo("/products");
+            }
+            else
+            {
+                Console.WriteLine("Login failed");
+            }
         }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Login error: {ex.Message}");
+        }
+    }
+
+    private void TogglePasswordVisibility()
+    {
+        showPassword = !showPassword;
     }
 }
