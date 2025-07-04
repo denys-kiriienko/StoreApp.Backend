be44b5a62e78b9885ced668fa5f8983981db216e
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-07-04 18:52:13 +0300

    Add ProductForm page

---
+++ StoreApp.Admin.Client/Components/Pages/Products/ProductForm/ProductFormPage.razor.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using StoreApp.Admin.Client.Services.Interfaces;
+using StoreApp.Shared.Models;
+
+namespace StoreApp.Admin.Client.Components.Pages.Products.ProductForm;
+
+public partial class ProductFormPage
+{
+    [Inject] public required NavigationManager NavigationManager { get; set; }
+    [Inject] public required IProductApiService ProductApiService { get; set; }
+
+    [Parameter] public int? Id { get; set; }
+
+    private bool IsEditMode => Id.HasValue;
+
+    private ProductModel? ProductModel { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        if (IsEditMode)
+        {
+            var product = await ProductApiService.GetProductByIdAsync(Id.Value);
+
+            if (product is null)
+            {
+                NavigationManager.NavigateTo("/products");
+                return;
+            }
+
+            ProductModel = product;
+        }
+        else
+        {
+            ProductModel = new ProductModel() { Name = string.Empty, Price = 0 };
+        }
+    }
+
+    private async Task HandleValidSubmit()
+    {
+        if (ProductModel is null)
+            return;
+
+        if (IsEditMode)
+        {
+            await ProductApiService.UpdateProductAsync(Id.Value, ProductModel);
+        }
+        else
+        {
+            await ProductApiService.AddProductAsync(ProductModel);
+        }
+
+        NavigationManager.NavigateTo("/products");
+    }
+
+    private async Task OnFileSelected(InputFileChangeEventArgs e)
+    {
+        var file = e.File;
+        if (file is not null)
+        {
+            using var stream = file.OpenReadStream();
+            using var ms = new MemoryStream();
+            await stream.CopyToAsync(ms);
+            ProductModel!.ImageData = ms.ToArray();
+        }
+    }
+
+    private void GoBack()
+    {
+        NavigationManager.NavigateTo("/products");
+    }
+}
--- StoreApp.Admin.Client/Components/Pages/Products/ProductsPage.razor.cs
+++ StoreApp.Admin.Client/Components/Pages/Products/ProductsPage.razor.cs
@@ -7,6 +7,7 @@ namespace StoreApp.Admin.Client.Components.Pages.Products;
 public partial class ProductsPage
 {
     [Inject] public required IProductApiService ProductApiService { get; set; }
+    [Inject] public required NavigationManager NavigationManager { get; set; }
 
     private List<ProductModel>? products;
 
@@ -17,14 +18,12 @@ public partial class ProductsPage
 
     private void OnAddProduct()
     {
-        // TODO: implement in future
-        Console.WriteLine("Add clicked");
+        NavigationManager.NavigateTo($"/product-form/");
     }
 
     private void OnEdit(ProductModel product)
     {
-        // TODO: implement in future
-        Console.WriteLine($"Edit: {product.Name}");
+        NavigationManager.NavigateTo($"/product-form/{product.Id}");
     }
 
     private void OnDelete(ProductModel product)
