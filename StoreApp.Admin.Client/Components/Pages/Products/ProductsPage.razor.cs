ac73583c9d410a9eda216fe638f9118c7ec51beb
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-07-04 13:34:25 +0300

    Refactor Home and Products pages

---
+++ StoreApp.Admin.Client/Components/Pages/Products/ProductsPage.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using StoreApp.Admin.Client.Services.Interfaces;
+using StoreApp.Shared.Models;
+
+namespace StoreApp.Admin.Client.Components.Pages.Products;
+
+public partial class ProductsPage
+{
+    [Inject] public required IProductApiService ProductApiService { get; set; }
+
+    private List<ProductModel>? products;
+
+    protected override async Task OnInitializedAsync()
+    {
+        products = await ProductApiService.GetAllProductsAsync();
+    }
+}
