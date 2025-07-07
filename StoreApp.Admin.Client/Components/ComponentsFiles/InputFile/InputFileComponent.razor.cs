a9fc2974f5516ab0cb0696cc6bd2b8f89784b142
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-07-07 11:29:40 +0300

    Add custom InputFile component

---
+++ StoreApp.Admin.Client/Components/ComponentsFiles/InputFile/InputFileComponent.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace StoreApp.Admin.Client.Components.ComponentsFiles.InputFile;
+
+public partial class InputFileComponent
+{
+    [Parameter] public string ButtonText { get; set; } = "Upload File";
+    [Parameter] public EventCallback<InputFileChangeEventArgs> OnFileSelectedCallback { get; set; }
+
+    private string? FileName;
+
+    private async Task OnFileSelected(InputFileChangeEventArgs e)
+    {
+        var file = e.File;
+        if (file is not null)
+        {
+            FileName = file.Name;
+        }
+
+        await OnFileSelectedCallback.InvokeAsync(e);
+    }
+}
