593c959cfa2eedcf28c3e6c6dc257bbc1ecdab4e
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-07-07 17:16:38 +0300

    Update ProductEntity to use ImageUrl instead of ImageData

--- StoreApp.DAL/Entities/ProductEntity.cs
+++ StoreApp.DAL/Entities/ProductEntity.cs
@@ -6,7 +6,7 @@ public class ProductEntity
     public required string Name { get; set; }
     public string? Description { get; set; }
     public required decimal Price { get; set; }
-    public byte[]? ImageData { get; set; }
+    public string? ImageUrl { get; set; }
 
     public ICollection<CartItemEntity> CartItems { get; set; } = new List<CartItemEntity>();
 }
