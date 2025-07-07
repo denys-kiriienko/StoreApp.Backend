a4560a87b23764d23b3693131847ed9ff3434830
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-07-07 18:18:38 +0300

    Refactor ProductCard component

--- StoreApp.Admin.Client/Components/Pages/Products/Components/ProductCard/ProductCardComponent.razor.cs
+++ StoreApp.Admin.Client/Components/Pages/Products/Components/ProductCard/ProductCardComponent.razor.cs
@@ -5,7 +5,15 @@ namespace StoreApp.Admin.Client.Components.Pages.Products.Components.ProductCard
 
 public partial class ProductCardComponent
 {
+    [Inject] public required IConfiguration Configuration { get; set; }
     [Parameter] public required ProductModel Product { get; set; }
     [Parameter] public required EventCallback<ProductModel> OnEdit { get; set; }
     [Parameter] public required EventCallback<ProductModel> OnDelete { get; set; }
+
+    private string BaseUrl = string.Empty;
+
+    protected override void OnInitialized()
+    {
+        BaseUrl = Configuration["ApiSettings:BaseUrl"] ?? "";
+    }
 }
