c3f2e074b4826319ea491ae4aad93c9b906b8096
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-07-07 13:00:54 +0300

    Add ProductCard component

---
+++ StoreApp.Admin.Client/Components/Pages/Products/Components/ProductCard/ProductCardComponent.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using StoreApp.Shared.Models;
+
+namespace StoreApp.Admin.Client.Components.Pages.Products.Components.ProductCard;
+
+public partial class ProductCardComponent
+{
+    [Parameter] public required ProductModel Product { get; set; }
+    [Parameter] public required EventCallback<ProductModel> OnEdit { get; set; }
+    [Parameter] public required EventCallback<ProductModel> OnDelete { get; set; }
+}
