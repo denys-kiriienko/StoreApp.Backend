aead5a59267ae11142b16fff10d6c082dd7da129
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-07-08 14:48:15 +0300

    Add new client project with start section

---
+++ StoreApp.Client/Components/SvgFiles/SvgStarComponent.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace StoreApp.Client.Components.SvgFiles;
+
+public partial class SvgStarComponent
+{
+    [Parameter] public string? Width { get; set; } = "48";
+    [Parameter] public string? Height {  get; set; } = "48";
+}
