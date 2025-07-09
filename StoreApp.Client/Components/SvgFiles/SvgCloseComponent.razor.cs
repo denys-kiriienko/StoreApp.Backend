29f6534f871fa6b8445bcaecce86a8dc9295f34b
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-07-09 13:26:56 +0300

    Add promo section to Home page

---
+++ StoreApp.Client/Components/SvgFiles/SvgCloseComponent.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace StoreApp.Client.Components.SvgFiles;
+
+public partial class SvgCloseComponent
+{
+    [Parameter] public string? Width { get; set; } = "14";
+    [Parameter] public string? Height { get; set; } = "14";
+}
