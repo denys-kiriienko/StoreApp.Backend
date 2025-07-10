f622ead06aaeaeab71d9792c16e35a555c296c61
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-07-10 13:26:40 +0300

    Add CardItem component

---
+++ StoreApp.Client/Components/ComponentFiles/CardItem/CardItemComponent.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace StoreApp.Client.Components.ComponentFiles.CardItem;
+
+public partial class CardItemComponent
+{
+    [Parameter] public string ImageSrc { get; set; } = string.Empty;
+    [Parameter] public string Title { get; set; } = string.Empty;
+    [Parameter] public double Rating { get; set; } = 0.0;
+    [Parameter] public string RatingText { get; set; } = string.Empty;
+    [Parameter] public string CurrentPrice { get; set; } = string.Empty;
+    [Parameter] public string? OldPrice { get; set; }
+    [Parameter] public string? Discount { get; set; }
+}
---
+++ StoreApp.Client/Components/ComponentFiles/SvgFiles/SvgRatingStarTrimmedComponent.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace StoreApp.Client.Components.ComponentFiles.SvgFiles;
+
+public partial class SvgRatingStarTrimmedComponent
+{
+    [Parameter] public string? Width { get; set; } = "9";
+    [Parameter] public string? Height { get; set; } = "17";
+}
