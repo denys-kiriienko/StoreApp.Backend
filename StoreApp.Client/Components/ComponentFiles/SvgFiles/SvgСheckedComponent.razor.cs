85bb021a8cfd906201a1072c92c795fd23714aef
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-07-11 16:44:11 +0300

    Add SvgChecked component

---
+++ StoreApp.Client/Components/ComponentFiles/SvgFiles/SvgСheckedComponent.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace StoreApp.Client.Components.ComponentFiles.SvgFiles;
+
+public partial class SvgСheckedComponent
+{
+    [Parameter] public string? Width { get; set; } = "20";
+    [Parameter] public string? Height { get; set; } = "21";
+}
