d99a5dc15680a88b4c5608afa6bd9d8cc5c71860
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-07-11 16:43:56 +0300

    Add SvgArrow component

---
+++ StoreApp.Client/Components/ComponentFiles/SvgFiles/SvgArrowComponent.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace StoreApp.Client.Components.ComponentFiles.SvgFiles;
+
+public partial class SvgArrowComponent
+{
+    [Parameter] public string? Width { get; set; } = "20";
+    [Parameter] public string? Height { get; set; } = "16";
+    [Parameter] public bool RevertIcon { get; set; }
+}
