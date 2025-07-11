7289c8932d5967c035172779887c7be30e83d448
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-07-11 16:45:29 +0300

    Add Customers component

---
+++ StoreApp.Client/Components/Pages/Home/Components/Customers/CustomersComponent.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace StoreApp.Client.Components.Pages.Home.Components.Customers;
+
+public partial class CustomersComponent
+{
+    [Inject] public required IJSRuntime JSRuntime { get; set; }
+    private ElementReference reviewsWrapperRef;
+
+    private async Task ScrollLeft()
+    {
+        await JSRuntime.InvokeVoidAsync("scrollReviews", reviewsWrapperRef, -425);
+    }
+
+    private async Task ScrollRight()
+    {
+        await JSRuntime.InvokeVoidAsync("scrollReviews", reviewsWrapperRef, 425);
+    }
+}
