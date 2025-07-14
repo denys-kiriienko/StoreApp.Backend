5a80c3ed114a54d17f4c70c0841fdd8c6e5ed92b
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-07-14 16:47:50 +0300

    Add BasicButton component

---
+++ StoreApp.Client/Components/ComponentFiles/Buttons/BasicButton/BasicButtonComponent.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace StoreApp.Client.Components.ComponentFiles.Buttons.BasicButton;
+
+public partial class BasicButtonComponent
+{
+    [Parameter] public string? Width { get; set; } = "100%";
+    [Parameter] public string? Height { get; set; } = "100%";
+    [Parameter] public string? Text { get; set; }
+    [Parameter] public string FontSize { get; set; } = "16px";
+    [Parameter] public bool IsRevert { get; set; }
+    [Parameter] public string? BackgroundColor { get; set; } = "#000000";
+    [Parameter] public bool Disabled { get; set; }
+    [Parameter] public EventCallback OnClick { get; set; }
+
+    protected override void OnInitialized() { }
+
+    private string GetButtonStyle()
+    {
+        return $"width: {Width}; height: {Height}; background-color: {BackgroundColor};";
+    }
+}
