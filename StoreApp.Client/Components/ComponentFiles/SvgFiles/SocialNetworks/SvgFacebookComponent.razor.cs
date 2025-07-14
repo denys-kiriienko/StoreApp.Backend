a85ac661992ddbfbd21e41ca31eaa731cd5d4d14
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-07-14 11:35:18 +0300

    Add social networks svg

---
+++ StoreApp.Client/Components/ComponentFiles/SvgFiles/SocialNetworks/SvgFacebookComponent.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace StoreApp.Client.Components.ComponentFiles.SvgFiles.SocialNetworks;
+
+public partial class SvgFacebookComponent
+{
+    [Parameter] public string? Width { get; set; } = "28";
+    [Parameter] public string? Height { get; set; } = "28";
+}
