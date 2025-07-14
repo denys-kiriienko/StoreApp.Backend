551dd5407701826b4e08d0be1bed544b8a9ae3bd
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-07-14 18:46:57 +0300

    Add BasicInput component

---
+++ StoreApp.Client/Components/ComponentFiles/Inputs/BasicInput/BasicInputComponent.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace StoreApp.Client.Components.ComponentFiles.Inputs.BasicInput;
+
+public partial class BasicInputComponent
+{
+    [Parameter] public string? Width { get; set; } = "100%";
+    [Parameter] public string? Height { get; set; } = "100%";
+    [Parameter] public string? Placeholder { get; set; }
+    [Parameter] public string FontSize { get; set; } = "16px";
+    [Parameter] public string? BackgroundColor { get; set; } = "#FFFFFF";
+    [Parameter] public RenderFragment? Icon { get; set; }
+    [Parameter] public RenderFragment? ChildContent { get; set; }
+    [Parameter] public string IconOpacity { get; set; } = "1.0";
+
+
+    private string GetWrapperStyle()
+    {
+        return $"width: {Width}; height: {Height}; background-color: {BackgroundColor};";
+    }
+
+    private string GetInputStyle()
+    {
+        return $"font-size: {FontSize};";
+    }
+}
