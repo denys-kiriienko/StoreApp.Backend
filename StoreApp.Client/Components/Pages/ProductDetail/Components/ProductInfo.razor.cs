b1a50cbc768f6d4d26829134371c1095a20b21ef
bashivan-lpnu <ivan.bash.pz.2022@lpnu.ua>
2025-07-18 17:24:11 +0300

    Refactor ProductInfo component: integrate SVG checkmark, remove default product values, and enhance responsive styles

--- StoreApp.Client/Components/Pages/ProductDetail/Components/ProductInfo.razor.cs
+++ StoreApp.Client/Components/Pages/ProductDetail/Components/ProductInfo.razor.cs
@@ -6,25 +6,7 @@ namespace StoreApp.Client.Components.Pages.ProductDetail.Components;
 public partial class ProductInfo : ComponentBase
 {
     [Parameter]
-    public ProductModel Product { get; set; } = new()
-    {
-        ImageSrc = "/images/card-item-1.png",
-        Title = "T-shirt with Tape Details",
-        Rating = 4.5,
-        CurrentPrice = "$120",
-        OldPrice = "$150",
-        Discount = "20%",
-        Description = "This graphic t-shirt which is perfect for any occasion. Crafted from a soft and breathable fabric, it offers superior comfort and style.",
-        UnitsInStock = 10,
-        Images =
-        [
-            "/images/card-item-1.png",
-            "/images/card-item-3.png",
-            "/images/card-item-4.png"
-        ],
-        Colors = ["#000000", "#ff0000", "#00ff00", "#0000ff"],
-        Sizes = ["Small", "Medium", "Large", "X-Large"]
-    };
+    public ProductModel Product { get; set; }
     
     public int SelectedColorIndex { get; set; } = 0;
 
