fe476647005aac090cfccf7914eb57ce96887ac5
bashivan-lpnu <ivan.bash.pz.2022@lpnu.ua>
2025-07-18 17:24:25 +0300

    Add ReviewList component with review display, filtering options, and load more functionality

--- StoreApp.Client/Components/Pages/ProductDetail/Components/ReviewList.razor.cs
+++ StoreApp.Client/Components/Pages/ProductDetail/Components/ReviewList.razor.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Components;
+using StoreApp.Client.Models;
 
 namespace StoreApp.Client.Components.Pages.ProductDetail.Components;
 
 public partial class ReviewList : ComponentBase
 {
+    [Parameter]
+    public List<ReviewModel> Reviews { get; set; } = [];
 }
