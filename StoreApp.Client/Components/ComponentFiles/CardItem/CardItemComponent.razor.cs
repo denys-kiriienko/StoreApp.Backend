46a89d9674a18e81aef42635f5e6b266625c6d56
bashivan-lpnu <ivan.bash.pz.2022@lpnu.ua>
2025-07-21 17:39:11 +0300

    Make CardItemComponent clickable to navigate to product details

--- StoreApp.Client/Components/ComponentFiles/CardItem/CardItemComponent.razor.cs
+++ StoreApp.Client/Components/ComponentFiles/CardItem/CardItemComponent.razor.cs
@@ -4,6 +4,9 @@ namespace StoreApp.Client.Components.ComponentFiles.CardItem;
 
 public partial class CardItemComponent
 {
+    [Inject] private NavigationManager navigationManager { get; set; } = default!;
+    
+    [Parameter] public int Id { get; set; }
     [Parameter] public string ImageSrc { get; set; } = string.Empty;
     [Parameter] public string Title { get; set; } = string.Empty;
     [Parameter] public double Rating { get; set; } = 0.0;
@@ -11,4 +14,9 @@ public partial class CardItemComponent
     [Parameter] public double CurrentPrice { get; set; }
     [Parameter] public double? OldPrice { get; set; }
     [Parameter] public double? Discount { get; set; }
+    
+    private void OnCardClick()
+    {
+        navigationManager.NavigateTo($"products/{Id}");
+    }
 }
