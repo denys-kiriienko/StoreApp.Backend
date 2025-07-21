c620259af7d6ab8171e373be05d675332aa45491
bashivan-lpnu <ivan.bash.pz.2022@lpnu.ua>
2025-07-21 21:45:50 +0300

    Remove default Review initialization in ProductReview component

--- StoreApp.Client/Components/Pages/ProductDetail/Components/ProductReview.razor.cs
+++ StoreApp.Client/Components/Pages/ProductDetail/Components/ProductReview.razor.cs
@@ -6,11 +6,5 @@ namespace StoreApp.Client.Components.Pages.ProductDetail.Components;
 public partial class ProductReview : ComponentBase
 {
     [Parameter]
-    public ReviewModel Review { get; set; } = new()
-    {
-        UserName = "John Doe",
-        Rating = 4.5,
-        Comment = "Great product! Highly recommend.",
-        CreatedAt = DateTime.Now.AddDays(-2)
-    };
+    public ReviewModel Review { get; set; }
 }
