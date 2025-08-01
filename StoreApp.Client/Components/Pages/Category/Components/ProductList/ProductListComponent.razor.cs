9e92fce0fa19e1dad85e9f3881f31e860a38fa24
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-08-01 18:29:22 +0300

    add product list component to category page

---
+++ StoreApp.Client/Components/Pages/Category/Components/ProductList/ProductListComponent.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using StoreApp.Client.Models;
+
+namespace StoreApp.Client.Components.Pages.Category.Components.ProductList;
+
+public partial class ProductListComponent
+{
+    [Parameter] public List<ProductModel> ProductList { get; set; } = new();
+    [Parameter] public string HeaderText { get; set; } = string.Empty;
+    [Parameter] public bool IsVisibleViewAllButton { get; set; } = true;
+}
