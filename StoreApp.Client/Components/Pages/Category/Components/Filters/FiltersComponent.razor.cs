3adde88f6567c50adf67e644b85286a38bf58395
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-08-12 14:55:46 +0300

    Add adaptive style for Category page

--- StoreApp.Client/Components/Pages/Category/Components/Filters/FiltersComponent.razor.cs
+++ StoreApp.Client/Components/Pages/Category/Components/Filters/FiltersComponent.razor.cs
@@ -1,5 +1,9 @@
-﻿namespace StoreApp.Client.Components.Pages.Category.Components.Filters;
+﻿using Microsoft.AspNetCore.Components;
+
+namespace StoreApp.Client.Components.Pages.Category.Components.Filters;
 
 public partial class FiltersComponent
 {
+    [Parameter] public bool IsMobileOverlay { get; set; } = false;
+    [Parameter] public EventCallback OnClose { get; set; }
 }
