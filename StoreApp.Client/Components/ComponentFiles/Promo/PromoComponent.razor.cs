a68fea70c446c90a656643156dab2e7a95723700
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-08-18 11:51:30 +0300

    Implement promo component visibility and dismissal logic

--- StoreApp.Client/Components/ComponentFiles/Promo/PromoComponent.razor.cs
+++ StoreApp.Client/Components/ComponentFiles/Promo/PromoComponent.razor.cs
@@ -1,5 +1,25 @@
-﻿namespace StoreApp.Client.Components.ComponentFiles.Promo;
+﻿using Microsoft.AspNetCore.Components;
+using StoreApp.Client.Services;
+using StoreApp.Shared.Constants;
+
+namespace StoreApp.Client.Components.ComponentFiles.Promo;
 
 public partial class PromoComponent
 {
+    [Inject] public required ILocalStorageService LocalStorage { get; set; }
+
+    private bool _isVisible;
+
+    protected override async Task OnInitializedAsync()
+    {
+        var dismissed = await LocalStorage.GetItemAsync<bool?>(UiConstants.LocalStorageKeys.PromoDismissed);
+        _isVisible = dismissed != true;
+    }
+
+    private async Task OnCloseClicked()
+    {
+        _isVisible = false;
+        await LocalStorage.SetItemAsync(UiConstants.LocalStorageKeys.PromoDismissed, true);
+        await InvokeAsync(StateHasChanged);
+    }
 }
---
+++ StoreApp.Shared/Constants/UiConstants.cs
@@ -0,0 +1,9 @@
+namespace StoreApp.Shared.Constants;
+
+public static class UiConstants
+{
+    public static class LocalStorageKeys
+    {
+        public const string PromoDismissed = "promo_dismissed";
+    }
+}
